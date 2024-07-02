c7e2a9e58a666516300b9753c67b8914580fc08b
yoshinaka-droid <yoshinaka012825@gmail.com>
2024-07-02 21:59:01 +0900

    ソート機能(未完成)

--- router/src/Components/Body.js
+++ router/src/Components/Body.js
@@ -6,9 +6,33 @@ import './Body.css';
 
 const Body = () => {
   const navigate = useNavigate();
+  // var NameList = List.slice();
+  // const asc = NameList.sort((a, b) => {
+    // if (a.Phonetic > b.Phonetic) {
+      // return 1;
+    // } else {
+      // return -1;
+    // }
+  // });
+  // const desc = () => {NameList.sort((a, b) => {
+    // if (a.Phonetic > b.Phonetic) {
+      // return -1;
+    // } else {
+      // return 1;
+    // }
+  // })};
   
   return (
     <div className="body">
+      <div className="prologue">
+      <button
+        className="sort"
+        // onClick={()=>{NameList=asc}}
+      >ソート</button>
+      <span>今日の夕食は何にする？(適当な文字列)</span>
+      {/* {console.log(asc[0])} */}
+      {/* {desc()} */}
+      </div>
       {List.map((rest) => {
         const total = () => {
           rest.Total=Math.round((rest.Taste+rest.Amount+rest.Price)*10/3)/10
